1252486baa5716690305005c37bf7169e467b096
Ivan Dmitrachkov <dmitrachkovivan@gmail.com>
2019-07-20 00:29:07 +0300

    add block: Advantages

--- src/components/AdvantagesBlock/AdvantagesBlock.js
+++ src/components/AdvantagesBlock/AdvantagesBlock.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import s from './AdvantagesBlock.module.scss'
 import ServiceTextBlock from '../ServiceTextBlock/ServiceTextBlock'
+import im1 from '../../static/clients.png'
+import im2 from '../../static/humans.png'
+import im3 from '../../static/soft.png'
 
 const items = [
   {
     title: 'Идем навстречу клиенту',
-    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы']
+    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы'],
+    img: im1
   },
   {
     title: 'Идем навстречу клиенту',
-    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы']
+    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы'],
+    img: im2
   },
   {
     title: 'Идем навстречу клиенту',
-    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы']
-  },
-  {
-    title: 'Идем навстречу клиенту',
-    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы']
+    items: ['Выслушаем и учтем ваше мнение', 'найдем компромиссы'],
+    img: im3
   }
 ]
 
@@ -30,8 +32,9 @@ const AdvantagesBlock = () => {
         {
           items.map((item, key) => {
             return (
-              <div key={key}>
+              <div key={key} className={(key % 2) ? s.flex : s.flex__invert}>
                 <ServiceTextBlock {...item} theme={'white'} />
+                <div className={s.img} style={{ backgroundImage: `url(${item.img})` }} />
               </div>
             )
           })
